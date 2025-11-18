654b1e4f17ae78df1ec1973587a2790508318957
vasudhasreecs24 <vasudhasree.cs24@bmsce.ac.in>
2025-11-18 11:37:00 +0530

    Update Internals.java

--- JAVALABBCS271/CIE/Internals.java
+++ JAVALABBCS271/CIE/Internals.java
@@ -1,15 +1,1 @@
-package CIE;
-import java.util.Scanner;
 
-public class Internals extends Student {
-    protected int marks[] = new int[5];
-
-    public void inputCIEmarks() {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter Internal Marks of 5 subjects:");
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Subject " + (i+1) + ": ");
-            marks[i] = s.nextInt();
-        }
-    }
-}
